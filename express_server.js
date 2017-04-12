90a85a3a7d20a43918141f2d02315e608c180fb1
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-12 15:49:04 +0000

    Functionality for shortening URLs is done.

--- express_server.js
+++ express_server.js
@@ -3,12 +3,24 @@ let express = require('express');
 let app = express();
 let PORT = process.env.PORT || 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "c1tI2c": "http://www.garatephotography.com"
 };
+
+const alphaNum = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function generateRandomString(length, chars) {
+    let result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+    return result;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -22,9 +34,24 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   var templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  let tempShort = generateRandomString(6, alphaNum);
+  urlDatabase[tempShort] = req.body.longURL;
+  res.redirect(`/urls/${tempShort}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
