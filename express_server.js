3310e5ca22b7f90a1b0c4061448c93912714fa1e
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-13 18:35:43 +0000

    Added code to the delete and edit endpoints to check if user_id exist and allow or block delete and edit operations.

--- express_server.js
+++ express_server.js
@@ -10,13 +10,16 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+const user_id = req.cookies["user_id"];
+
 // Using EJS as a template engine
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "c1tI2c": "http://www.garatephotography.com"
+  "c1tI2c": "http://www.garatephotography.com",
+  "8e3tv1": "http://www.garateca.com"
 };
 
 const users = {
@@ -24,13 +27,19 @@ const users = {
     id: "userRandomID",
     email: "user@example.com",
     password: "123",
-    urls: ["9sm5xK"]
+    shorturls: ["9sm5xK"]
   },
  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "dishwasher-funk",
-    urls: []
+    shorturls: []
+  },
+  "W3Rtzs9ja1": {
+    id: "W3Rtzs9ja1",
+    email: "carlos.m.garate@gmail.com",
+    password: "qwerty",
+    shorturls: ["c1tI2c","8e3tv1"]
   }
 }
 
@@ -94,8 +103,6 @@ app.get("/users.json", (req, res) => {
 // Render the template to create a new URL
 app.get("/urls/new", (req, res) => {
   // Read the cookie and send the user id object to the _header
-  let user_id = req.cookies["user_id"];
-
   if (user_id === undefined) {
     res.redirect("/login");
   } else {
@@ -109,7 +116,7 @@ app.get("/urls/new", (req, res) => {
 // Renders the index
 app.get("/urls", (req, res) => {
   // Read the cookie and send the user id object to the _header
-  let user_id = req.cookies["user_id"];
+
   let templateVars = {
     urls: urlDatabase,
     user_id: users[user_id]
@@ -127,8 +134,13 @@ app.post("/urls", (req, res) => {
 
 // Receives the request to delete a URL. Deletes the key and redirects to home.
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  if (users[user_id].shorturl === req.params.id) {
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(403);
+  }
+
 });
 
 // Gets a URL given a key and Redirects to the URL.
@@ -139,7 +151,6 @@ app.get("/u/:shortURL", (req, res) => {
 
 // Displays the info and update page of a given URL's key.
 app.get("/urls/:id", (req, res) => {
-  let user_id = req.cookies["user_id"];
   let templateVars = {
     shortURL: req.params.id,
     urls: urlDatabase,
@@ -150,8 +161,12 @@ app.get("/urls/:id", (req, res) => {
 
 // Receives the request to update an existing URL, inserts the update and redirects to home.
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect("/urls");
+  if (users[user_id].shorturl === req.params.id) {
+    urlDatabase[req.params.id] = req.body.longURL;
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // Receives the request to login, creates a cookie with the user received and redirects to home.
@@ -160,12 +175,12 @@ app.post("/login", (req, res) => {
   let reqPassword = req.body.password;
   let reqEmail = req.body.email;
   // Get the user id cookie
-  let setCookie = req.cookies["user_id"];
+  let setCookie = user_id;
   // validate password and email
   if (emailExists(users, reqEmail) && validEmailPassword(users, reqEmail, reqPassword)) {
       // Password and email exist but there's no cookie.
       // Get the userID as it is in the datasource and use it to set the cookie value.
-      if (Object.keys(req.cookies).length === 0 || req.cookies["user_id"] === 'undefined' ) {
+      if (Object.keys(req.cookies).length === 0 || user_id === 'undefined' ) {
         res.cookie("user_id", getUserID(users, reqEmail, reqPassword));
         res.redirect("/urls");
       // Otherwise cookie is fine just redirect.
